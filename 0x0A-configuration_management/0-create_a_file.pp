7d343d294ed92182a6318eb2650b5ab696dbd848
Breign3 <brainy.ngwenya@gmail.com>
2024-06-28 12:52:26 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# create file 
+#create file
 file {'/tmp/school':
-	mode    => '0744',
-	owner   => 'www-data',
-	group   => 'www-data',
-	content => 'I love Puppet',
-	ensure  => present
+ensure  => present,
+content => 'I love Puppet',
+group   => 'www-data',
+owner   => 'www-data',
+mode    => '0744'
 }
